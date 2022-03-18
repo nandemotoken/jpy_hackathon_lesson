7c9832361763eb7b4706c397286e025620afc1e5
nandemotoken <46263082+nandemotoken@users.noreply.github.com>
2022-03-18 19:24:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 async function myFunction(){
     const provider = await new ethers.providers.Web3Provider(window.ethereum)
-    await provider.getBlockNumber();
+    const blocknumber = await provider.getBlockNumber();
+    alert(blocknumber);
+    await ethereum.request({ method: 'eth_requestAccounts' });
     const signer = await provider.getSigner();
-    alert(signer.getAddress());
+    const address = signer.getAddress();
+    alert(address);
 }
 
 window.onload = ()=>{
